a3ec40202a90a691ebb65257684921a42167c228
ASTROGBAE <gabebrett@iinet.net.au>
2021-05-16 12:37:31 +1000

    added percent utilities method

--- src/Attackers.java
+++ src/Attackers.java
@@ -4,20 +4,9 @@ public class Attackers extends Utilities implements Event {
 
     // TODO implement constructor (threat modifier?)
 
-    // TODO create a generic chance thing
-    // calculate chance for town to be attacked (in percentages)
-    public boolean isAttacked() {
-        int attackChance = 10;
-        int randomPerc = randInt(0, 101);
-        if (randomPerc < attackChance) {
-            return true;
-        }
-        return false;
-    }
-
     // town is attacked! lose between 1 and 5 production (makes it more interesting)
     public double attack() {
-        if (isAttacked()) {
+        if (getPercentChance(10)) {
             return randInt(10, 50);
         }
         return 0;
--- src/Utilities.java
+++ src/Utilities.java
@@ -5,10 +5,22 @@ import java.util.concurrent.ThreadLocalRandom;
 public abstract class Utilities {
 
     int randInt(int min, int max) {
-        return ThreadLocalRandom.current().nextInt(min, max+1);
+        return ThreadLocalRandom.current().nextInt(min, max);
     }
 
     double randDouble(double min, double max) {
-        return ThreadLocalRandom.current().nextDouble(min, max+1);
+        return ThreadLocalRandom.current().nextDouble(min, max);
     } 
+
+    double getNoise(double st) {
+        return st*ThreadLocalRandom.current().nextGaussian();
+    }
+
+    boolean getPercentChance(int chance) {
+        int roll = randInt(1, 101);
+        if (roll < chance) {
+            return false;
+        }
+        return true;
+    }
 }
